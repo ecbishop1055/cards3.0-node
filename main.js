cef16174f9652f4321ec4a4d1c9e63d1e8305177
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2021-04-12 11:00:20 -0500

    express, template setups

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-const port = 3000,
+const port = process.env.PORT || 3000,
     express = require("express"),
     app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, Universe!");
+const path = require('path');
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+// })
+
+app.use(express.static(path.join(__dirname, 'templates')))
+
+app.get("/definitions", (req,res) => {
+  res.send("Define")
 })
+
 .listen(port, () => {
     console.log(`The express.js server has started and is listening on port number: ${port}`);
 });
