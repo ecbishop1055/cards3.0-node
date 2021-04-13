72e48adb157f36d37c4931585ec49b840a9d5276
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2021-04-13 10:47:39 -0500

    ignore package.json

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const path = require("path");
 const pug = require("pug");
+const bodyParser = require("body-parser")
 const MongoClient = require('mongodb').MongoClient
 const connectionString = 'mongodb+srv://admin:admin@cluster0.xbdzk.mongodb.net/cluster0-shard-00-01.xbdzk.mongodb.net:27017?retryWrites=true&w=majority'
 
@@ -48,6 +49,15 @@ app.get('/home', (req, res) => {
       res.render('home',{
       });
 });
+
+// Post
+app.post('/login', async (req,res) => {
+  console.log(req.body)
+  res.json({status : 'ok '})
+})
+
+app.use(bodyParser.json());
+
 app.listen(3000, () => {
   console.log("server started on port 3000");
 });
